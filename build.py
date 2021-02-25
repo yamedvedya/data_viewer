e994db0ea9eb47d58188a00aaa5e868bdfb006c3
p23user <p23user@desy.de>
2021-02-25 17:34:35 +0100

    aaa

--- build.py
+++ build.py
@@ -10,9 +10,11 @@ in_dirs = ["src/uis"]
 out_dirs = ["src/gui"]
 
 ui_compilers = {"linux2": "python -m PyQt5.uic.pyuic",
+                "linux": "python -m PyQt5.uic.pyuic",
                 "win32": "C://Users//matveyev//AppData//Local//Programs//Python//Python37-32//Scripts//pyuic5.exe"}
 
-rc_compilers = {"linux2": "python -m PyQt5.uic.pyrcc",
+rc_compilers = {"linux2": "pyrcc5",
+                "linux": "pyrcc5",
                 "win32":  "C://Users//matveyev//AppData//Local//Programs//Python//Python37-32//Scripts//pyrcc5.exe"}
 
 # ----------------------------------------------------------------------
