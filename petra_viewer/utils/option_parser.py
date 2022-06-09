4db07748db9c58759b4311ca4fe81280e0763aba
karnem <mikhail.karnevskiy@desy.de>
2022-06-09 14:37:04 +0200

    Add more clear help message to option -p

--- petra_viewer/utils/option_parser.py
+++ petra_viewer/utils/option_parser.py
@@ -4,7 +4,7 @@ from optparse import OptionParser
 def get_options(args=None):
     parser = OptionParser()
 
-    parser.add_option("-p", "--profile", dest='profile', default='default', help="profile selection")
+    parser.add_option("-p", "--profile", dest='profile', default='default', help="Path to config file.")
 
     parser.add_option("--tests", action='store_true', dest='test', default=False, help="print logs to console")
     parser.add_option("--log", action='store_true', dest='log', help="print logs to console")
